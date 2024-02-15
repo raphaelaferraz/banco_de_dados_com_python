8bfe1417dc2efd744ea1262714aad429275467a5
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-02-15 13:22:11 -0300

    feature: Add select all

--- conexao.py
+++ conexao.py
@@ -5,11 +5,18 @@ cursor = conexao.cursor()
 
 # execução de comandos
 #cursor.execute('CREATE TABLE alunos(id INT, nome VARCHAR(150), idade INT, curso VARCHAR(150))')
-cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (1, "Raphaela", 19, "Eng. de Software")')
-cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (2, "Victor", 19, "Eng. de Software")')
-cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (3, "Ana", 19, "Ciência da Computação")')
-cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (4, "Duda", 19, "Ciências C.")')
-cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (5, "Davi", 19, "Sistemas de Informação")')
+# cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (1, "Raphaela", 19, "Eng. de Software")')
+# cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (2, "Victor", 19, "Eng. de Software")')
+# cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (3, "Ana", 19, "Ciência da Computação")')
+# cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (4, "Duda", 19, "Ciências C.")')
+# cursor.execute('INSERT INTO alunos(id, nome, idade, curso) VALUES (5, "Davi", 19, "Sistemas de Informação")')
+
+# Visualizar todos os alunos 
+dados = cursor.execute('SELECT * FROM alunos')
+for alunos in dados:
+  print(alunos)
+
+
 
 conexao.commit()  # envio de informações
 conexao.close()  # fechamento da conexão com o banco de dados
